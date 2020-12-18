ac9f8a40cd103de886072ffa2f9fbb02a656fcc2
IonZource <dao47@live.com>
2020-12-18 15:07:12 -0600

    Added first unit test for operationcomplete

--- UnitTestsForQuizApp2/QuestionTests.cs
+++ UnitTestsForQuizApp2/QuestionTests.cs
@@ -3,6 +3,8 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuizApp2;
+
 
 namespace UnitTestsForQuizApp2
 {
@@ -10,13 +12,18 @@ namespace UnitTestsForQuizApp2
     /// Summary description for UnitTest1
     /// </summary>
     [TestClass]
-    public class UnitTest1
+    public class QuestionTests
     {
-        public UnitTest1()
+        [TestMethod]
+        public void CheckCorrectAnswerChoice()
         {
             //
             // TODO: Add constructor logic here
             //
+            //using QuizApp2
+            Assert.AreEqual("OperationComplete", QuizApp2.EventNames.OperationComplete);   
+                
+            
         }
 
         private TestContext testContextInstance;
